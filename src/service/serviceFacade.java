1150c32f8b6903babcb8956c821d311ebfe3ffed
João Calapez <joao.calapez.c@gmail.com>
2020-01-06 03:50:01 +0000

    Revert "Completed contact and group methods in EntitiesFacade."
    This reverts commit dbbaed6b716d87dc252b1388dae66f3a5d148e60.

--- src/service/serviceFacade.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-public class serviceFacade {
-	
-	public static void sendMessage(String FromNumber, String toNumber, String finalMessage) {
-		WhatsAppSenderHandler.sendText(FromNumber, toNumber, finalMessage);
-	}
-
-}
