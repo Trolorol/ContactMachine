759e1a215d6dcdb1a6dd3e89b866cf5a377792ed
João Calapez <joao.calapez.c@gmail.com>
2020-01-14 04:34:12 +0000

    Added first GUI templates. Added Controllers for GUI interface.

--- src/gui/MainWindowController.java
+++ src/gui/MainWindowController.java
@@ -1,15 +1,51 @@
 package gui;
 
+import java.util.LinkedHashSet;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class MainWindowController {
 	
 	@FXML
-	private Button btTest;
+	private Button management;
+	private Button send;
+	private ListView<String> contactList;
+	private ListView<String> groupList;
+	private ListView<String> templateList;
+	private TextField message;
+	
+	
+	
 	
 	@FXML
-	public void onBtTestAction() {
+	public void btManagementAction() {
 		System.out.println("Click");
 	}
+	
+	@FXML
+	public void btSendAction() {
+		
+	}
+	
+	@FXML
+	
+	public void showContactList() {
+		
+	}
+	
+	@FXML
+	public void showGroupList() {
+		
+	}
+	
+	@FXML
+	public void showTemplateList() {
+		
+	}
+	
+	
+	
 }
